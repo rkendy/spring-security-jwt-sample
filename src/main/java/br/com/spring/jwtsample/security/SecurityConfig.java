288e653d3216488bc0c289013a09ad2d19acfd3f
Roberto Kendy Sawamura <rkendy@gmail.com>
2019-09-30 15:27:49 -0300

    Remocao de @Configuration desnecessaria

--- src/main/java/br/com/spring/jwtsample/security/SecurityConfig.java
+++ src/main/java/br/com/spring/jwtsample/security/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
